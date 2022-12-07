68c41ffb2454ed7b32949c4c9955685d274309a8
Gabriel <gopxfs@hotmail.com>
2022-12-07 02:56:09 -0300

    Add rubocop & fix errors

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 class Transaction < ApplicationRecord
   belongs_to :user
 
   validates :type, presence: true
-  validates :value, presence: true,  numericality: { greater_than_or_equal_to: 0 }
+  validates :value, presence: true, numericality: { greater_than_or_equal_to: 0 }
 end
