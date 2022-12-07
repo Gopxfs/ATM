22f4f678f4dc7be9ed027324fbbe8d422683faab
Gabriel <gopxfs@hotmail.com>
2022-12-07 02:50:53 -0300

    Create models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :validatable
+
+  validates :name, presence: true, length: { minimum: 0, maximum: 100 }
+  validates :balance, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :account, presence: true, numericality: { greater_than_or_equal_to: 0 }
 end
